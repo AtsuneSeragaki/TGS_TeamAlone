c2c8834c67c1a1447d2994139df404352c9b9dd8
AtsuneSeragaki <120348226+AtsuneSeragaki@users.noreply.github.com>
2024-08-05 14:58:52 +0900

    update

--- tgs_alone/Scene/InputRankingScene.cpp
+++ tgs_alone/Scene/InputRankingScene.cpp
@@ -226,18 +226,40 @@ void InputRankingScene::Draw() const
 	{// 名前が入力されていないとき
 
 		// 名前を入力してくださいの文字を描画
-		DrawGraph(0, -37, img[4], TRUE);
+		//DrawGraph(0, -37, img[4], TRUE);
+		/*DrawGraph(530 - 35, 63, font_img[0], TRUE);
+		DrawGraph(590 - 35, 63, font_img[1], TRUE);
+		DrawGraph(650 - 35, 63, font_img[2], TRUE);
+		DrawGraph(710 - 35, 63, font_img[3], TRUE);
+		DrawGraph(770 - 35, 63, font_img[4], TRUE);*/
 	}
 	else
 	{
+		for (int i = 0; i <= name_num; i++)
+		{
+			DrawFormatString(0 + i * 30, 0, 0x000000, "%d", (int)name[i]);
+		}
+
+		DrawFormatString(0, 30, 0x000000, "%d", name_num);
+
 		// 入力された名前を描画
 		if (input_end == false)
-		{
-			DrawFormatStringToHandle(630 - name_num * 30, 63, 0x000000, font, "%s", name);
+		{	
+			for (int i = 0; i <= name_num; i++)
+			{
+				DrawGraph((605 - (name_num * 27))+ i * 60, 63, font_img[(int)name[i] - 65], TRUE);
+			}
+			
+			//DrawFormatStringToHandle(630 - name_num * 30, 63, 0x000000, font, "%s", name);
 		}
 		else
 		{
-			DrawFormatStringToHandle(630 - (name_num - 1) * 30, 63, 0x000000, font, "%s", name);
+			for (int i = 0; i < name_num; i++)
+			{
+				DrawGraph(497 + i * 60, 63, font_img[(int)name[i] - 65], TRUE);
+			}
+
+			//DrawFormatStringToHandle(630 - (name_num - 1) * 30, 63, 0x000000, font, "%s", name);
 		}
 	}
 
